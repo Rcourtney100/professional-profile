a20419a8021c4ef2789352c0b9418c956179abd1
Ryan Courtney <ryan.courtney1@gmail.com>
2017-02-15 13:08:05 -0500

    initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,17 @@
+class HomeController < ApplicationController
+  def about
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def portfolio
+  end
+
+  def contact
+  	
+  end
+end
---
+++ test/controllers/home_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class HomeControllerTest < ActionDispatch::IntegrationTest
+  test "should get about" do
+    get home_about_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get home_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get home_show_url
+    assert_response :success
+  end
+
+  test "should get portfolio" do
+    get home_portfolio_url
+    assert_response :success
+  end
+
+end
