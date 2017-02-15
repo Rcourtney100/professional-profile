126b2c488b8b396548e6e5ad86b592a31355d7bb
Ryan Courtney <ryan.courtney1@gmail.com>
2017-02-15 13:24:37 -0500

    added basic navbar to layout

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get 'home/portfolio'
 
+  get 'home/contact'
+
   root to: 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
